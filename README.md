54907d1d7bd06f48c3ee83362aaf8af5e63fb034
Sam Slater <sslater@posteo.net>
2024-08-11 22:15:01 +0000

    Start safe concurrency examples. Finish no_concurrency.go

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ Examples of fundamental concepts, syntax, functions, features, etc.
 
 golang_builder.sh - used to build go files for several mArchs and os 
 
-### Examples 1-4
+## Examples 1-4
 
 Introduce basic syntax, basic fmt, os, log package usage 
 
-### Example 5
+## Example 5
 
 Add ioutil . more log, and bufio, including: ioutil.ReadFile() , ioutil.WriteFile() , log.Fatalf , log.Panicf (and log.Panicln() ) 
 Also bufio.NewReader(os.Stdin) , defer keyword, inputReader.Readstring()
@@ -17,4 +17,32 @@ Also bufio.NewReader(os.Stdin) , defer keyword, inputReader.Readstring()
 
 Shows how to setup and use a logger using logrus, with a custom formatter for INFO/WARN/DEBUG/ and custom timestamp. 
 
+## Safe Concurrency
 
+Here I try to demonstrate methods of concurrency that (hopefully) should avoid race conditions / deadlocks / resource leaking . Ths might come at the cost of some CPU overhead. We will use the same logger / formatter used above. Please note that for this section I won't be going over mutexes or waitgroups here. They are fundamental concurrency concepts, but the aim of this section is to be safe, not to be comprehensive. I will demonstrate them later. I'll also give more information on patterns at a later point.  Here  I demonstrate:
+
+### No concurrency
+ 
+See noConcurrency.go - Just copying one directory to another. No concurrency. Baseline.
+
+### TODO Channels with Goroutines
+
+see exampleGoroutine.go , exampleChannel.go , and of naturally , exampleChannelsWithGoroutines.go .
+
+Goroutines and Channels by themselves are used as examples. Channels with goroutines are used together in safety because (to my understanding):
+
+1) No explicit locking - avoiding the use of mutexes (which can cause deadlocks / race conditions)
+2) Synchronization - Channels sync access to shared data, so only a single goroutine processes data at a time.
+3) Blocking - Send/receive operations to/from a channel blocks a goroutine UNTIL the process can proceed. If done correctly, this should prevent race conditions.
+
+### TODO Errgroups with Contexts
+
+see exampleErrgroupsWithContexts.go
+
+In short, contexts make it easy to manage/propgate cancellations/timeouts across goroutines. Propogation is sending cancellation signals / timeouts / deadlines to goroutines derived from it. Errgroups make it easy to launch goroutines and capture errors produced by them. Combining the two allows a group of goroutines to be cancelled in the case of a fail/timeout/deadline . 
+
+### TODO Worker Pools 
+
+see exampleWorkerPool.go
+
+Not difficult to understand - you can limit the number of concurrent. In my opinion, because of encapsulation, it is easier to write/understand/manage. 
---
+++ safe_concurency/noConcurrency.go
@@ -0,0 +1,132 @@
+package main
+
+//For this purpose we're assuming you have the directory srcDir with files in it
+// Also assumes you have the directory destDir to copy the files to
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/otiai10/copy"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	srcDir     = "/home/sslater/exampleFiles"
+	destDir    = "/home/users/sslater/golang_snippets_learning/safe_concurency/filesgohere"
+	logdir     = filepath.Join(destDir, "logs")
+	logfile    = filepath.Join(logdir, "logfile.txt")
+	mainLogger *logrus.Logger
+)
+
+type CustomFormatter struct{}
+
+func (f *CustomFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	loc, err := time.LoadLocation("America/Los_Angeles")
+	if err != nil {
+		return nil, fmt.Errorf("error loading location: %v", err)
+	}
+	entry.Time = entry.Time.In(loc)
+	timestamp := entry.Time.Format("2006-01-02 - 3:04PM (PST)")
+
+	level := entry.Level.String()
+	switch entry.Level {
+	case logrus.InfoLevel:
+		level = "[INFO]"
+	case logrus.WarnLevel:
+		level = "[WARN]"
+	case logrus.ErrorLevel:
+		level = "[ERROR]"
+	case logrus.FatalLevel:
+		level = "[FATAL]"
+	case logrus.PanicLevel:
+		level = "[PANIC]"
+	case logrus.DebugLevel:
+		level = "[DEBUG]"
+	default:
+		level = "[UNKNOWN]"
+	}
+
+	msg := fmt.Sprintf("%s %s %s\n", timestamp, level, entry.Message)
+	return []byte(msg), nil
+}
+
+func setupLogging(logFile string) *logrus.Logger {
+	mainLogger := logrus.New()
+
+	mainLogFile, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("Error opening main log file: %v\n", err)
+		os.Exit(1)
+	}
+
+	mainLogger.SetOutput(mainLogFile)
+	mainLogger.SetLevel(logrus.DebugLevel)
+	mainLogger.SetFormatter(&CustomFormatter{})
+
+	return mainLogger
+}
+
+func copySrcToDest(srcDir, destDir string, mainLogger *logrus.Logger) {
+
+	//check source and dest directories exist
+
+	if _, err := os.Stat(srcDir); os.IsNotExist(err) {
+		mainLogger.Errorf("Source directory %s does not exist: %v\n", srcDir, err)
+		return
+	} else if err != nil {
+		mainLogger.Errorf("Error checking source directory %s: %v\n", srcDir, err)
+		return
+	}
+
+	if _, err := os.Stat(destDir); os.IsNotExist(err) {
+		mainLogger.Errorf("Destination directory %s does not exist: %v\n", destDir, err)
+		return
+	} else if err != nil {
+		mainLogger.Errorf("Error checking destination directory %s: %v\n", destDir, err)
+		return
+	}
+
+	//walk through src
+	err := filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			mainLogger.Errorf("Error accessing path %s: %v\n", path, err)
+			return err
+		}
+
+		// Create corresponding path in destination directory
+		relPath, err := filepath.Rel(srcDir, path)
+		if err != nil {
+			mainLogger.Errorf("Error determining relative path for %s: %v\n", path, err)
+			return err
+		}
+		destPath := filepath.Join(destDir, relPath)
+
+		// Check if dir or file in src
+		if info.IsDir() {
+			// Create dir if it is a directory
+			if err := os.MkdirAll(destPath, info.Mode()); err != nil {
+				mainLogger.Errorf("Error creating directory %s: %v\n", destPath, err)
+				return err
+			}
+		} else {
+			// Copy file if file
+			if err := copy.Copy(path, destPath); err != nil {
+				mainLogger.Errorf("Error copying file %s to %s: %v\n", path, destPath, err)
+				return err
+			}
+			mainLogger.Infof("Copied file %s to %s\n", path, destPath)
+
+		}
+		return nil
+	})
+
+	if err != nil {
+		mainLogger.Errorf("Error walking through source directory %s: %v\n", srcDir, err)
+	} else {
+		mainLogger.Infof("Successfully copied all files from %s to %s\n", srcDir, destDir)
+	}
+
+}
